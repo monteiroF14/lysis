29bf4d5dda940e17c6af64ec9e362dd3b7d7790f
unknown <bluueem@gmail.com>
2023-10-12 17:46:27 +0100

    feat: adding eslint

---
+++ backend/.eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: [".eslintrc.{js,cjs}"],
+			parserOptions: {
+				sourceType: "script",
+			},
+		},
+	],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	plugins: ["@typescript-eslint"],
+	rules: {
+		"@typescript-eslint/no-explicit-any": "off",
+		"@typescript-eslint/consistent-type-imports": [
+			"warn",
+			{
+				prefer: "type-imports",
+				fixStyle: "inline-type-imports",
+			},
+		],
+		"@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+	},
+};
