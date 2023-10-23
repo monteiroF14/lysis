cf1a5e8d68380c180062263cde38b5350a7d8b5c
André Monteiro <bluueem@gmail.com>
2023-10-23 16:06:31 +0100

    fix: dotenv was undefined

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,11 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "./types/database/types";
+import dotenv from "dotenv";
 
-const supabase = createClient<Database>(
-	process.env.SUPABASE_URL || "https://spnvuzhvpjgamwhhbhwd.supabase.co",
-	process.env.SUPABASE_API_KEY ||
-		"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNwbnZ1emh2cGpnYW13aGhiaHdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc1NDI3OTUsImV4cCI6MjAwMzExODc5NX0.3cLmtlj9pZjdGdEeEf6aY_WDfHeDX5y-biv8ubReoOE"
-);
+dotenv.config();
+
+const supabase = createClient<Database>(process.env.SUPABASE_URL!, process.env.SUPABASE_API_KEY!);
 
 export default {
 	server: {
