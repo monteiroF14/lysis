6bd6f1819b48e9627976e9b6077be43e65ef5eb6
unknown <bluueem@gmail.com>
2023-10-26 02:46:22 +0100

    refactor: checkJwt and checkPermission

---
+++ backend/src/middleware/checkJwt.ts
@@ -0,0 +1,38 @@
+import { createJwtToken } from "utils/createJwtToken";
+import type { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import UserModel from "services/UserService";
+import type { JwtPayload } from "types/JwtPayload";
+
+export async function checkJwt(req: Request, res: Response, next: NextFunction) {
+	const authHeader = req.header("Authorization");
+	const token = authHeader?.split(" ")[1];
+
+	if (!token) {
+		return res.status(401).json({ message: "Access denied. No token provided." });
+	}
+
+	try {
+		const jwtPayload = jwt.verify(token, process.env.JWT_SECRET!);
+		req.jwtPayload = jwtPayload as JwtPayload;
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: "Failed to authenticate." });
+	}
+
+	try {
+		const newToken = createJwtToken(req.jwtPayload);
+
+		await UserModel.addRefreshTokenToUser({
+			token: newToken,
+			id: req.jwtPayload.id,
+		});
+
+		res.header("Authorization", `Bearer ${newToken}`);
+		res.cookie("refreshToken", newToken, { httpOnly: true });
+
+		return next();
+	} catch {
+		return res.status(401).json({ message: "Access denied. No token provided." });
+	}
+}
--- backend/src/middleware/checkPermission.ts
+++ backend/src/middleware/checkPermission.ts
@@ -1,20 +1,15 @@
-import type { PERMISSIONS } from "config/permissions";
+import { ROLE_PERMISSIONS, type PERMISSIONS } from "config/permissions";
 import type { Request, Response, NextFunction } from "express";
-import Admin from "types/user/Admin";
-import User from "types/user/User";
+import type { JwtPayload } from "types/JwtPayload";
 
 export function checkPermission(permissions: PERMISSIONS[]) {
 	return (req: Request, res: Response, next: NextFunction) => {
-		const user = req.user;
+		const { role } = req.jwtPayload as JwtPayload;
 
 		for (const permission of permissions) {
-			if (user instanceof Admin && user.hasPermission(permission)) {
-				next();
-			} else if (user instanceof User && user.hasPermission(permission)) {
-				next();
-			} else {
-				return res.status(403).json({ error: "Permission denied" });
-			}
+			const userHasPermission = ROLE_PERMISSIONS[role].includes(permission);
+
+			return userHasPermission ? next() : res.status(401).json({ error: "Unauthorized" });
 		}
 	};
 }
