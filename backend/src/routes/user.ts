a8aed8fcacc2971323468a4fda5f14b4860cddb6
unknown <bluueem@gmail.com>
2023-10-26 02:49:32 +0100

    refactor: removing the updateUserRole from user controller

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { PERMISSIONS } from "config/permissions";
-import { createUser, deleteUser, getAllUsers, getUserById, updateUserRole } from "controllers/user";
+import { createUser, deleteUser, getAllUsers, getUserById } from "controllers/user";
 import { checkPermission } from "middleware/checkPermission";
 
 const router = Router();
@@ -14,7 +14,5 @@ router
 	.route("/:id")
 	.get(checkPermission([PERMISSIONS.USERS_READ]), getUserById)
 	.delete(checkPermission([PERMISSIONS.USERS_DELETE]), deleteUser);
-
-router.route("/:id/role").post(checkPermission([PERMISSIONS.USERS_ROLE_UPDATE]), updateUserRole);
 
 export default router;
