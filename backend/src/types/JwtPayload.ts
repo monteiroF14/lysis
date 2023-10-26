2610ddc85b169da43e5cd3c7e67ce4dcaf7e0e02
unknown <bluueem@gmail.com>
2023-10-26 02:49:57 +0100

    feat: adding JwtPayload type

---
+++ backend/src/types/JwtPayload.ts
@@ -0,0 +1,9 @@
+import type { SYSTEM_ROLES } from "config/permissions";
+import type Email from "../utils/types/Email";
+
+export type JwtPayload = {
+	id: string;
+	username: string;
+	email: Email;
+	role: keyof typeof SYSTEM_ROLES;
+};
