f6915959e6fbd011a0170ebd9257a5b4cd3ed593
unknown <bluueem@gmail.com>
2023-10-26 02:50:50 +0100

    refactor: adding utils folder

---
+++ backend/src/utils/types/Password.ts
@@ -0,0 +1,47 @@
+import bcrypt from "bcrypt";
+
+class Password {
+	private readonly password: string;
+
+	private constructor(password: string) {
+		this.password = password;
+		Object.freeze(this);
+	}
+
+	static create(password: string): Password {
+		if (!Password.validate(password)) {
+			throw new Error(`Invalid password: ${password}`);
+		}
+		return new Password(password);
+	}
+
+	get value(): string {
+		return this.password;
+	}
+
+	static validate(password: string): boolean {
+		if (password.length < 8) {
+			return false;
+		}
+
+		if (!/[A-Z]/.test(password)) {
+			return false;
+		}
+
+		if (!/[a-z]/.test(password)) {
+			return false;
+		}
+
+		if (!/[0-9]/.test(password)) {
+			return false;
+		}
+
+		return true;
+	}
+
+	static compare(userPassword: string, candidatePassword: string): Promise<boolean> {
+		return bcrypt.compare(candidatePassword, userPassword);
+	}
+}
+
+export default Password;
