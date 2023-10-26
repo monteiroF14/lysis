c304d86f1df6fc3f7e18982a9e88f4decc83b56d
unknown <bluueem@gmail.com>
2023-10-26 19:36:54 +0100

    refactor: dotenv/config

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,17 +1,18 @@
 import express from "express";
 import type { Express } from "express";
-import dotenv from "dotenv";
 import config from "config";
-import cors from "cors";
 import router from "routes";
-
-dotenv.config();
+import cors from "cors";
+import helmet from "helmet";
+import "dotenv/config";
 
 const app: Express = express();
 const PORT = config.server.port;
 
-app.use(express.json());
 app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(router);
 
